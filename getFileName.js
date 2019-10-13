94408eb8b1de829c7dd2009656c5248148c9a23a
surunzi <surunzi@foxmail.com>
2019-10-13 18:43:51 +0800

    chore: smallChanges

--- getFileName.js
+++ getFileName.js
@@ -1,12 +1,17 @@
 /* Extract file name from url.
  */
 
-_('last trim')
+_('last trim Url')
 
 exports = function (url) {
   let ret = last(url.split('/'))
 
   if (ret.indexOf('?') > -1) ret = trim(ret.split('?')[0])
 
-  return ret === '' ? 'unknown' : ret
+  if (ret === '') {
+    url = new Url(url)
+    ret = url.hostname
+  }
+
+  return ret
 }
